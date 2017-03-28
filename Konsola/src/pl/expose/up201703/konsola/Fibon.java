6442eec7f9a02919e252bc7f8768ff1b5baf0ec4
szkolenie-softronic <w3@epf.pl>
2017-03-28 21:13:39 +0200

    rekursja: duża silnia

---
+++ Konsola/src/pl/expose/up201703/konsola/Fibon.java
@@ -0,0 +1,39 @@
+package pl.expose.up201703.konsola;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Fibon {
+
+	static BigInteger[] bufor;
+	
+	public static void main(String[] args) {
+		System.out.println("Start");
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		bufor = new BigInteger[n+1];
+		
+		BigInteger s = fibon(n);
+		System.out.println(s);
+		
+		scanner.close();
+		System.out.println("End.");
+	}
+	
+	static BigInteger fibon(int n){
+		if(bufor[n] != null){
+			return bufor[n];
+		}
+
+		BigInteger s = new BigInteger(""+n);
+		if(n>1){
+			s = new BigInteger("0").add(fibon(n-1)).add(fibon(n-2));
+		}
+		bufor[n] = s;
+		return s;
+	}
+	
+
+}
