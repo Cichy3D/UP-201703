987a82f9e20f6a708fbeeaa008c9f2af099a99d6
szkolenie-softronic <w3@epf.pl>
2017-04-24 20:50:12 +0200

    LightsOut 2

--- Okna/src/pl/expose/up201703/okna/zdarzenia/LightsOut.java
+++ Okna/src/pl/expose/up201703/okna/zdarzenia/LightsOut.java
@@ -3,11 +3,8 @@ package pl.expose.up201703.okna.zdarzenia;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.Random;
 
 import javax.swing.JButton;
